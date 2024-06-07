a5e1537078f8bcaa2cf6e2b3e53e89fa265983f7
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-06-07 01:52:35 +0100

    add progressBar for video upload

--- src/components/uploadVideo/Upload.jsx
+++ src/components/uploadVideo/Upload.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styles from './Upload.module.css'
 import * as videoServices from '../../services/videoServices'
+import { endpoints } from '../../lib/endpoints';
 
 
 const UploadVideo = () => {
@@ -23,27 +24,62 @@ const UploadVideo = () => {
     const [game, setGame] = useState('')
     const [video, setVideo] = useState()
 
+    const [uploadProgress, setUploadProgress] = useState(0);
 
-
-
-
-    const uploadSubmitHandler = async (e) => {
-
+    const uploadSubmitHandler = (e) => {
         e.preventDefault()
-        const formData = new FormData()
-        // for (const key in formValue) {
-        //     formData.append(key, formValue[key])
-        // }
+        if (!video) return;
 
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
         formData.append('title', title)
         formData.append('description', description)
         formData.append('game', game)
         formData.append('video', video)
 
-        await videoServices.upload(formData)
+        xhr.upload.addEventListener('progress', (event) => {
+            console.log(event.loaded, event.total);
+            if (event.lengthComputable) {
+                const percentComplete = (event.loaded / event.total) * 100;
+                setUploadProgress(percentComplete);
+            }
+        });
+
+        xhr.upload.addEventListener('load', () => {
+            console.log('Upload complete');
+            setUploadProgress(100);
+        });
+        xhr.upload.addEventListener('error', () => {
+            console.error('Upload failed');
+            setUploadProgress(0);
+        });
+     
+        xhr.upload.addEventListener('abort', () => {
+            console.error('Upload Aborted');
+            setUploadProgress(0);
+        });
+     
+
+        xhr.open('POST', `http://localhost:3000${endpoints.upload}`);
+        xhr.send(formData);
+    };
+    // const uploadSubmitHandler = async (e) => {
+
+    //     e.preventDefault()
+    //     const formData = new FormData()
+    //     // for (const key in formValue) {
+    //     //     formData.append(key, formValue[key])
+    //     // }
+
+    //     formData.append('title', title)
+    //     formData.append('description', description)
+    //     formData.append('game', game)
+    //     formData.append('video', video)
+
+    //     await videoServices.upload(formData)
 
 
-    }
+    // }
 
     return (
 
@@ -52,7 +88,7 @@ const UploadVideo = () => {
                 <input type="text" name="title" placeholder='Title' onChange={e => setTitle(e.target.value)} value={title} />
                 <textarea name="description" id="description" placeholder='Description' onChange={e => setDescription(e.target.value)} value={description}></textarea>
 
-                <select name="game" id="game" onChange={e => setGame(e.target.value)} value={game}>
+                <select name="game" id="game" onChange={e => setGame(e.currentTarget.value)} value={game}>
                     <option value="Valorant">Valorant</option>
                     <option value="Counter Strike 2">Counter Strike 2</option>
                     <option value="League of Legends">League of Legends</option>
@@ -62,8 +98,31 @@ const UploadVideo = () => {
                     <option value="Apex Legends">Apex Legends</option>
                 </select>
                 <input type="file" name="video" onChange={(e) => { setVideo(e.target.files[0]) }} />
-                <button>Submit</button>
+
+
+                {uploadProgress > 0 && (
+                    <div style={{ width: '100%', backgroundColor: '#f3f3f3', marginTop: '10px' }}>
+                        <div
+                            style={{
+                                width: `${uploadProgress}%`,
+                                height: '20px',
+                                backgroundColor: uploadProgress === 100 ? 'green' : 'blue',
+                                transition: 'width 0.2s',
+                            }}
+                        />
+                    </div>
+                )}
+
+
+
+                {uploadProgress > 0 && (<p>{Math.round(uploadProgress)}%</p>)}
+                {uploadProgress == 100 && (<a>Go to Video Page</a>)}
+                {uploadProgress === 0 && (<button>Submit</button>)}
+
+
             </form>
+
+
         </div>
     )
 }
