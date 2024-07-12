d6f3ac385aa6ed57ce45aa50ff258ac4e98363dc
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-07-12 12:21:05 +0100

    Update timeDifferenceToString.js

--- src/utils/timeDifferenceToString.js
+++ src/utils/timeDifferenceToString.js
@@ -1,11 +1,10 @@
-function timeDifferenceToString(createdAt) {
+ export default function timeDifferenceToString(createdAt) {
     const createdAtTime = new Date(createdAt);
     const currentTime = new Date();
 
-    // Calculate the difference in milliseconds
     const timeDifference = currentTime - createdAtTime;
 
-    // Calculate days, hours, minutes, and seconds
+    
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
@@ -22,8 +21,3 @@ function timeDifferenceToString(createdAt) {
 
     return timeDiffStr;
 }
-
-// Test the function
-const createdAtTestStr = "2024-07-06T15:45:08.832Z";
-const timeDifferenceStr = timeDifferenceToString(createdAtTestStr);
-console.log(timeDifferenceStr);
